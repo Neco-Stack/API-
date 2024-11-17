2cc64f72d998a254c9fb1e3ebf73304205a6f892
Neco-Stack <necoabay@googlemail.com>
2024-11-17 19:13:54 +0100

    postman

--- src/api/Interfaces/IStarWarsPlanets.ts
+++ src/api/Interfaces/IStarWarsPlanets.ts
@@ -20,3 +20,4 @@ export interface PlanetsResponse {
     previous?: string; 
     results: Planets[]; 
 }
+
---
+++ src/api/star-wars.ts
@@ -0,0 +1,9 @@
+// der Aufruf hat 2.12s gedauert
+// ich bekomme 60 Planeten als Antwort 
+// "name": "Coruscant", vorletzter Planet
+// das climate von Hoth ist frozen 
+// name des zweiten planeten auf seite 3: "corellia"
+// Aufgabe 2
+// die erste person in der antwort ist "Luke Skywalker"
+// sein Raumschiff heißt "X-wing", "Imperial shuttle"
+// "hair_color": "auburn, white", eye color: blue-gray
